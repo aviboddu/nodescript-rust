549977f04fc79e0e6df38d1533cabdce718f0905
aviboddu <aviboddu@gmail.com>
2025-03-09 15:20:16 -0700

    Remove test module

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,24 +1,21 @@
-#[cfg(test)]
-mod tests {
-    use nodescript_rust::*;
-    use std::path::Path;
+use nodescript_rust::*;
+use std::path::Path;
 
-    #[test]
-    fn basic_parse() {
-        let path = Path::new("./data/Minimal/Minimal.ns");
-        let result = tokenize_file(path).unwrap();
-        assert_eq!(
-            result,
-            tokens::Tokens {
-                tokens: vec![vec![
-                    tokens::Token::Print,
-                    tokens::Token::Number("0".into()),
-                    tokens::Token::Comma,
-                    tokens::Token::Identifier("input".into()),
-                    tokens::Token::Eof,
-                ],],
-                code: "PRINT 0, input".into(),
-            }
-        );
-    }
+#[test]
+fn basic_parse() {
+    let path = Path::new("./data/Minimal/Minimal.ns");
+    let result = tokenize_file(path).unwrap();
+    assert_eq!(
+        result,
+        tokens::Tokens {
+            tokens: vec![vec![
+                tokens::Token::Print,
+                tokens::Token::Number("0".into()),
+                tokens::Token::Comma,
+                tokens::Token::Identifier("input".into()),
+                tokens::Token::Eof,
+            ],],
+            code: "PRINT 0, input".into(),
+        }
+    );
 }
